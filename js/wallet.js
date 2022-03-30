f91e8fca9bb9190e9a60661c1239c0a758cd777c
marcusnftpolygon <marcusnft.polygon@gmail.com>
2022-03-30 15:32:42 -0400

    Connect Phantom

--- js/wallet.js
+++ js/wallet.js
@@ -1,29 +1,8 @@
-async function connectMM() {
-  try {
-    await ethereum.request({
-      method: "wallet_switchEthereumChain",
-      params: [{ chainId: "0x89" }],
-    });
-  } catch (switchError) {
-    if (switchError.code === 4902) {
-      try {
-        await ethereum.request({
-          method: "wallet_addEthereumChain",
-          params: [
-            {
-              chainId: "0x89",
-              rpcUrls: ["https://polygon-rpc.com"],
-              chainName: "Polygon Mainnet",
-              nativeCurrency: {
-                name: "MATIC",
-                symbol: "MATIC",
-                decimals: 18,
-              },
-              blockExplorerUrls: ["https://polygonscan.com/"],
-            },
-          ],
-        });
-      } catch (addError) {}
-    }
+async function connectPT() {
+  const isPhantomInstalled = window.solana && window.solana.isPhantom;
+  if (isPhantomInstalled) {
+    try {
+      await window.solana.connect();
+    } catch (err) {}
   }
 }
